1d8b7916e30d355d6579e44345218b5bb8eac9de
ggroce <ggrocedev@gmail.com>
2021-02-19 19:25:17 -0600

    further card styling

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -7,13 +7,11 @@ const Card = ({ name, type, DOB, awards, status, sex }) => {
     <div className="flip-wrapper">
       <div className="flip-wrapper-inner">
 
-
-      <article className="rabbit-card">
-
+        <article className="card-front stitching">
           <img src="https://lh3.googleusercontent.com/869xDoDxyUSqaxZxcpldiNHvO75HU4gg-lLEb-8U8uXtspgOjVhCY5Ff_jeujufZwoCJuDRrPPYKg8VW0RBgaIhWkITqqlGhTEAwO_g3dRtUbAAswH0T8RfEKMf6DQGFCGo3yJZMFQ" 
             loading="lazy" title="Photo of a rabbit" />
-          <h2>{name}</h2>
-          <div className="rabbit-card-front">
+          <div className="card-front-details">
+            <h2>{name}</h2>
             <hr />
             <p><strong>Type: </strong>{type}</p>
             <p><strong>DOB: </strong>{DOB}</p>
@@ -21,8 +19,14 @@ const Card = ({ name, type, DOB, awards, status, sex }) => {
             <p><strong>Status: </strong>{status}</p>
             <p><strong>Sex: </strong>{sex}</p>
           </div>
-
-      </article>
+        </article>
+        <article className="card-back stitching">
+          <div className="card-back-details">
+            <h2>{name}</h2>
+            <hr />
+            <p>We can put even more information on the back of the card here.  </p>
+          </div>
+        </article>
       
       </div>
     </div>

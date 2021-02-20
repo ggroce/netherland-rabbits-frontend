dbfce371d0a842f5d88bcde6cf9e6d49dfa11a59
Garrett Groce <ggroce@gmail.com>
2021-02-20 17:25:38 -0600

    fixing scrollbar jump onclick bug; reconfigured header css and spacing

--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -1,15 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
 import Header from '../components/Header';
 import Sidebar from '../components/Sidebar/Sidebar';
 import Footer from '../components/Footer';
-
-const SelectedPageParent = styled.div`
-  background: #B5CCB8;
-  margin-left: 175px;
-  margin-top: 50px;
-  flex: 1;
-`;
 
 function Layout(props) {
   return(
@@ -17,6 +9,7 @@ function Layout(props) {
       <Header />
       <Sidebar history={props.history}/>
       <main>
+      <div className="header-hidden-spacer" />
       <div className="scroll-wrapper">
         <div>
           {props.children}

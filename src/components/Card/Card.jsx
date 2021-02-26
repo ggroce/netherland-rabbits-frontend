9059f5cc7bb4007dfe9ee8164b6780b13271a400
ggroce <ggrocedev@gmail.com>
2021-02-25 23:51:08 -0600

    fully separated form into separate component

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,40 +1,19 @@
 import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { sendInquiryForm } from '../../actions'
 import 'react-responsive-modal/styles.css';
 import { Modal } from 'react-responsive-modal';
 import './Card.css';
-// import InquiryForm from '../InquiryForm/InquiryForm.jsx';
-// import InquiryForm from '../InquiryForm/InquiryForm';
+import InquiryForm from '../InquiryForm/InquiryForm.jsx';
 
 const Card = ({ rabbitName, rabbitType, rabbitDOB, rabbitAwards, rabbitStatus, rabbitSex }) => {
   const [open, setOpen] = useState(false);
 
   const onOpenModal = () => setOpen(true);
-  const onCloseModal = () => { 
-    setOpen(false);
-    setCustEmail('');
-    setCustName('');
-    setCustMessage('');
-  }
+  const onCloseModal = () => setOpen(false);
 
  const rotateCard = (e) => {
     if (e.target.id !== 'inquire-button') {
       e.currentTarget.classList.toggle('card--flipped');
     } 
-  }
-
-  const [custName, setCustName] = useState('');
-  const [custEmail, setCustEmail] = useState('');
-  const [custMessage, setCustMessage] = useState('');
-
-  const { isPending, err } = useSelector((state) => state.sendInquiryForm);
-
-  const dispatch = useDispatch();
-  const onSendInquiryForm = (e) => {
-    e.preventDefault();
-    dispatch(sendInquiryForm(custName, custEmail, custMessage, rabbitName));
-    onCloseModal();
   }
 
   return(
@@ -71,65 +50,8 @@ const Card = ({ rabbitName, rabbitType, rabbitDOB, rabbitAwards, rabbitStatus, r
       </div>
     </div>
 
-
-
-
     <Modal open={open} onClose={() => setOpen(false)} center>
-
-
-
-      <h4 style={{marginTop: ".7rem", marginBottom: "0"}}>Interested in {rabbitName}?</h4>
-      <h5 style={{marginTop: "0", marginLeft: "1rem", marginBottom: "0"}}>Please let us know!</h5>
-      <form>
-          <p>
-            <label htmlFor="name">
-              Name: <br />
-              <input 
-                type="text" 
-                id="name" 
-                value={custName}
-                onChange={e => setCustName(e.target.value)} 
-                required/>
-            </label>
-          </p>
-          <p>
-            <label htmlFor="email">
-              Email address: <br /> 
-              <input 
-                type="email" 
-                id="email" 
-                value={custEmail}
-                onChange={e => setCustEmail(e.target.value)} 
-                required/>
-            </label>
-          </p>
-          <p>
-            <label htmlFor="message">
-              <strong style={{marginBottom: "0", fontSize: ".85rem"}}>Share a bit about yourself 
-                <br />
-                (area you're from, experience with rabbits, etc):  
-                <br />
-              </strong>
-              <textarea 
-                type="text" 
-                rows="5" 
-                id="message" 
-                value={custMessage}
-                onChange={e => setCustMessage(e.target.value)} />
-            </label>
-            <br />
-            <strong style={{marginBottom: "0", fontSize: ".85rem"}}>
-            Please be sure and read our pet and shipping policy!
-            </strong>
-          </p>
-
-          <button onClick={(e) => onSendInquiryForm(e)}>Submit</button>
-          {/* <input onClick={() => onSendInquiryForm()} type="submit" value="Submit" /> */}
-          <br />
-        </form>
-
-
-      {/* <InquiryForm name={name}/> */}
+      <InquiryForm rabbitName={rabbitName} onCloseModal={onCloseModal}/>
     </Modal>
     </>
   );
--- src/components/InquiryForm/InquiryForm.jsx
+++ src/components/InquiryForm/InquiryForm.jsx
@@ -2,10 +2,9 @@ import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { sendInquiryForm } from '../../actions'
 
-function InquiryForm({ name }) {
-  const [open, setOpen] = useState(false);
+function InquiryForm({ rabbitName, onCloseModal }) {
+  // const [open, setOpen] = useState(false);
 
-  let rabbitName = name;
   const [custName, setCustName] = useState('');
   const [custEmail, setCustEmail] = useState('');
   const [custMessage, setCustMessage] = useState('');
@@ -14,14 +13,17 @@ function InquiryForm({ name }) {
 
   const dispatch = useDispatch();
   const onSendInquiryForm = (e) => {
+    if (!custName || !custEmail) {
+      return;
+    }
     e.preventDefault();
     dispatch(sendInquiryForm(custName, custEmail, custMessage, rabbitName));
-    setOpen(false);
+    onCloseModal();
   }
 
   return(
     <>
-      <h4 style={{marginTop: ".7rem", marginBottom: "0"}}>Interested in {name}?</h4>
+      <h4 style={{marginTop: ".7rem", marginBottom: "0"}}>Interested in {rabbitName}?</h4>
       <h5 style={{marginTop: "0", marginLeft: "1rem", marginBottom: "0"}}>Please let us know!</h5>
       <form>
           <p>
@@ -60,12 +62,16 @@ function InquiryForm({ name }) {
                 value={custMessage}
                 onChange={e => setCustMessage(e.target.value)} />
             </label>
+            <br />
+            <strong style={{marginBottom: "0", fontSize: ".85rem"}}>
+            Please be sure and read our pet and shipping policy!
+            </strong>
           </p>
+
           <button onClick={(e) => onSendInquiryForm(e)}>Submit</button>
           {/* <input onClick={() => onSendInquiryForm()} type="submit" value="Submit" /> */}
           <br />
-          Please be sure and read our pet and shipping policy!
-        </form>
+      </form>
     </>
   );
 }
